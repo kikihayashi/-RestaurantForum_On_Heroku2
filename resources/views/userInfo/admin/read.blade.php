aa62306b0e63150e09718b72caf63a456bbf9d63
Woody.Lin 林鈺衡 <tk70008@regalscan.com>
2021-11-18 10:54:42 +0800

    餐廳資訊版面修改

--- resources/views/userInfo/admin/read.blade.php
+++ resources/views/userInfo/admin/read.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <section style="margin-left:5%;">
   <h1>{{$thisRestaurant->name}}</h1>
-  營業時間:{{$thisRestaurant->opening_hour}}<br>
-  電話: {{$thisRestaurant->tel}}<br>
-  地址: {{$thisRestaurant->address}}<br>
-  描述:{{$thisRestaurant->content}}<br>
+  營業時間：{{$thisRestaurant->opening_hour}}<br>
+  電話：{{$thisRestaurant->tel}}<br>
+  地址：{{$thisRestaurant->address}}<br>
+  描述：{{$thisRestaurant->content}}<br>
   種類：{{$thisRestaurant->category_name}}<br>
   <a href="{{route('UserInfoController.showRestaurant')}}">回到所有餐廳</a>
 </section>
